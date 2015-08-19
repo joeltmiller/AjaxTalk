548b6980172df18466725a3a85a7c21397ec5f34
Joel Miller <joelmiller@Joels-MacBook-Pro.local>
2015-08-19 16:39:13 -0500

    initial commit

--- app.js
+++ app.js
@@ -3,4 +3,22 @@
  */
 $(document).ready(function() {
 
+
+    $.ajax("insert.html", {
+        success: function(response){
+            $(".myDiv").html(response);
+        },
+        error: function(){
+            alert('There was an error when requesting your file!');
+        },
+        timeout: 3000,
+        data: {"id" : "afs3reet3r4yrrt353"},
+        //beforeSend: function(){
+        //    alert("Before the request!");
+        //},
+        //complete: function(){
+        //    alert("Request complete!")
+        //}
+    })
+
 })
